b10ea9abb414ec897fe06d9b8674562d8820f6f5
Thejeswar <finance@wisdomtoolz.com>
2025-02-14 22:07:39 +0530

    updated

---
+++ Practise/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import CheckBoxDelete from './CheckBoxDelete'
+import AutoComplete from './AutoComplete'
+import Timer from './Timer'
+import Pagination from './Pagination'
+function App() {
+
+  return (
+    <>
+    Hello
+     <Pagination />
+    </>
+  )
+}
+
+export default App
